d5c817c0b5a521b4154004a92f8adac5ee3395b7
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-08-29 22:09:41 +0300

    Added Hilt Application and modified app style

---
+++ app/src/main/java/com/vickikbt/leadersboard/di/NetworkModule.kt
@@ -0,0 +1,11 @@
+package com.vickikbt.leadersboard.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@InstallIn(ApplicationComponent::class)
+
+@Module
+object NetworkModule {
+}
