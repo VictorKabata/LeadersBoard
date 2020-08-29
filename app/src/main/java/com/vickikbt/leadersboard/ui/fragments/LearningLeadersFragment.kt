fd0d2885dc8c8a352eddad415965a99043ed7909
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-08-29 20:52:52 +0300

    Removed boiler plate code from learning leader fragment

--- app/src/main/java/com/vickikbt/leadersboard/ui/fragments/LearningLeadersFragment.kt
+++ app/src/main/java/com/vickikbt/leadersboard/ui/fragments/LearningLeadersFragment.kt
@@ -6,55 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.vickikbt.leadersboard.R
-import com.vickikbt.leadersboard.ui.viewmodels.MainViewModel
 
-/**
- * A placeholder fragment containing a simple view.
- */
 class LearningLeadersFragment : Fragment() {
 
-    private lateinit var mainViewModel: MainViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java).apply {
-            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
-        }
-    }
-
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_learning_leaders, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
-        mainViewModel.text.observe(this, Observer<String> {
-            textView.text = it
-        })
+
+
         return root
     }
 
-    companion object {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private const val ARG_SECTION_NUMBER = "section_number"
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        @JvmStatic
-        fun newInstance(sectionNumber: Int): LearningLeadersFragment {
-            return LearningLeadersFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_SECTION_NUMBER, sectionNumber)
-                }
-            }
-        }
-    }
 }
