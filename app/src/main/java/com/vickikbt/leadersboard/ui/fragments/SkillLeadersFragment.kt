60425d68c899d3ed1824ca053a0fabdd3afa99d2
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-08-29 20:44:20 +0300

    Added second fragment

---
+++ app/src/main/java/com/vickikbt/leadersboard/ui/fragments/SkillLeadersFragment.kt
@@ -0,0 +1,22 @@
+package com.vickikbt.leadersboard.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vickikbt.leadersboard.R
+
+class SkillLeadersFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_skill_leaders, container, false)
+
+        return root
+    }
+
+}
