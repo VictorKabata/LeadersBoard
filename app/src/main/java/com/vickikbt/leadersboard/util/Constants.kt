d5829008b68410a2973f1aca9a638b311f76a651
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-08-29 22:05:59 +0300

    Finished making the project structure.

---
+++ app/src/main/java/com/vickikbt/leadersboard/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.vickikbt.leadersboard.util
+
+object Constants {
+
+    const val BASE_URL="https://gadsapi.herokuapp.com/api/"
+
+    const val HOURS_ENDPOINT="hours"
+
+    const val SKILLIQ_ENDPOINT="skilliq"
+
+}
