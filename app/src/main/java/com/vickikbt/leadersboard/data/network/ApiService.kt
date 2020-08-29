aed80f0f3c8990d90dfdb66d1c74e1eed2459d6b
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-08-29 22:27:47 +0300

    Update ApiService.kt

--- app/src/main/java/com/vickikbt/leadersboard/data/network/ApiService.kt
+++ app/src/main/java/com/vickikbt/leadersboard/data/network/ApiService.kt
@@ -1,4 +1,16 @@
 package com.vickikbt.leadersboard.data.network
 
+import com.vickikbt.leadersboard.model.HoursLeaderModel
+import com.vickikbt.leadersboard.model.SkillLeadersModel
+import com.vickikbt.leadersboard.util.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiService {
+
+    @GET(Constants.HOURS_ENDPOINT)
+    suspend fun fetchHoursLeaders(): Response<HoursLeaderModel>
+
+    @GET(Constants.HOURS_ENDPOINT)
+    suspend fun fetchSkillLeaders(): Response<SkillLeadersModel>
 }
