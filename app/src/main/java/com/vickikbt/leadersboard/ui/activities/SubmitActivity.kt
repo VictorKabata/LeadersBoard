5352b9c8ed70c4915c13f7a55a4abb972df909da
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-09-01 02:00:42 +0300

    Replaced submit fragment with activity instead

---
+++ app/src/main/java/com/vickikbt/leadersboard/ui/activities/SubmitActivity.kt
@@ -0,0 +1,12 @@
+package com.vickikbt.leadersboard.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vickikbt.leadersboard.R
+
+class SubmitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit)
+    }
+}
