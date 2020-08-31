ebe4a83769ee3e2c009ca502a8e166e0a6c4f010
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-08-31 14:52:07 +0300

    Finished designing the Skill IQ Leader Fragement

---
+++ app/src/main/java/com/vickikbt/leadersboard/ui/adapters/SkillLeadersRecyclerViewAdapter.kt
@@ -0,0 +1,41 @@
+package com.vickikbt.leadersboard.ui.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.vickikbt.leadersboard.R
+import com.vickikbt.leadersboard.model.SkillLeadersModel
+
+class SkillLeadersRecyclerViewAdapter(
+    private val context: Context,
+    private val skillLeadersList: List<SkillLeadersModel>
+) : RecyclerView.Adapter<SkillRecyclerViewViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SkillRecyclerViewViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.skill_recyclerview_item, parent, false)
+        return SkillRecyclerViewViewHolder(view)
+    }
+
+    override fun getItemCount() = skillLeadersList.size
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: SkillRecyclerViewViewHolder, position: Int) {
+        val leader = skillLeadersList[position]
+        Glide.with(context).load(leader.badgeUrl).into(holder.badge)
+        holder.userName.text = leader.name
+        holder.skillCountry.text = "${leader.score.toString()} skill IQ Score, ${leader.country}"
+    }
+}
+
+class SkillRecyclerViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val badge: ImageView = itemView.findViewById(R.id.skill_badge_imageView)
+    val userName: TextView = itemView.findViewById(R.id.skill_username_textView)
+    val skillCountry: TextView = itemView.findViewById(R.id.skill_country_textView)
+}
--- app/src/main/java/com/vickikbt/leadersboard/ui/fragments/SkillLeadersFragment.kt
+++ app/src/main/java/com/vickikbt/leadersboard/ui/fragments/SkillLeadersFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.vickikbt.leadersboard.R
+import com.vickikbt.leadersboard.model.SkillLeadersModel
+import com.vickikbt.leadersboard.ui.adapters.SkillLeadersRecyclerViewAdapter
 import com.vickikbt.leadersboard.ui.viewmodels.MainViewModel
 import com.vickikbt.leadersboard.util.Coroutines
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,20 +20,29 @@ class SkillLeadersFragment : Fragment() {
 
     private val viewModel by viewModels<MainViewModel>()
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_skill_leaders, container, false)
 
-        Coroutines.main {
-            viewModel.skillLeaders.await().observe(viewLifecycleOwner, Observer {
-                blank_textView.text = it.toString()
-            })
-        }
+        initRecyclerView()
 
         return root
+    }
+
+    private fun initRecyclerView() {
+        val skillList = arrayListOf<SkillLeadersModel>()
+        val adapter = SkillLeadersRecyclerViewAdapter(requireActivity(), skillList)
+
+        Coroutines.main {
+            viewModel.skillLeaders.await().observe(viewLifecycleOwner, Observer {
+                for (i in it.indices) {
+                    skillList.add(it[i])
+                    skill_recyclerview.adapter = adapter
+                }
+            })
+        }
     }
 
 }
